08886913d160efe3b477089adab1b9a1e442bb52
redorain <13991216410@163.com>
2021-04-21 11:16:31 +0800

    20201.4.21

--- 训练联盟5/f.cpp
+++ 训练联盟5/f.cpp
@@ -1,12 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = 1e5 + 5;
+int b[maxn], vis[maxn];
+
+priority_queue<int> q[maxn];
+struct node {
+
+    int val;
+    bool operator<(const node y) const {
+        return val > y.val;
+    }
+} a[maxn];
 
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
-    for(int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {
         int x, y;
         scanf("%d %d", &x, &y);
+        a[x].val++;
+        a[y].val++;
+        q[x].push(a[y].val);
+        q[y].push(a[x].val);
     }
+    sort(a + 1, a + n + 1);
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (vis[i])
+            continue;
+        else {
+            vis[i] = 1;
+            int temp = q[i].top();
+            while (vis[temp] == 1 && q[i].empty() == 0) {
+                q[i].pop();
+                temp = q[i].top();
+            }
+            if (vis[temp] == 0)
+                ans++, vis[temp] = 1;
+        }
+    }
+    printf("%d\n", ans);
     return 0;
 }
--- 训练联盟5/h.cpp
+++ 训练联盟5/h.cpp
@@ -2,62 +2,97 @@
 using namespace std;
 const int maxn = 1e5 + 5;
 #define ll long long
-struct node {
+struct node
+{
   ll num, len;
   string s;
 } a[maxn];
 
-int main() {
+int main()
+{
   int n;
   ios::sync_with_stdio(false);
   cin >> n;
   bool flag = 1;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cin >> a[i].s;
     a[i].len = a[i].s.size();
-    if (!flag) continue;
-    if (i == 0) {
+    if (!flag)
+      continue;
+    if (i == 0)
+    {
       for (int j = 0; j < a[i].len; j++)
-        if (a[i].s[j] == '9') a[i].s[j] = '6';
-    } else {
-      if (a[i].len < a[i - 1].len) {
+        if (a[i].s[j] == '9')
+          a[i].s[j] = '6';
+    }
+    else
+    {
+      if (a[i].len < a[i - 1].len)
+      {
         flag = 0;
         continue;
-      } else if (a[i].len > a[i - 1].len) {
+      }
+      else if (a[i].len > a[i - 1].len)
+      {
         for (int j = 0; j < a[i].len; j++)
-          if (a[i].s[j] == '9') a[i].s[j] = '6';
+          if (a[i].s[j] == '9')
+            a[i].s[j] = '6';
         continue;
-      } else {
+      }
+      else
+      {
         int pos = -1;
-        for (int j = 0; j < a[i].len; j++) {
-          if (a[i].s[j] == a[i - 1].s[j]) {
-            if (a[i].s[j] == '6') pos = j;
-          } else {
-            if (a[i].s[j] > a[i - 1].s[j]) {
-              if (a[i].s[j] == '9' && a[i - 1].s[j] == '6') {
+        for (int j = 0; j < a[i].len; j++)
+        {
+          if (a[i].s[j] == a[i - 1].s[j])
+          {
+            if (a[i].s[j] == '6')
+              pos = j;
+          }
+          else
+          {
+            if (a[i].s[j] > a[i - 1].s[j])
+            {
+              if (a[i].s[j] == '9' && a[i - 1].s[j] == '6')
+              {
                 a[i].s[j] = '6';
                 pos = j;
                 continue;
               }
-              if(a[i].s[j] == '9' && a[i - 1].s[j] < '6') a[i].s[j] = '6';
+              if (a[i].s[j] == '9' && a[i - 1].s[j] < '6')
+                a[i].s[j] = '6';
               for (int k = j + 1; k < a[i].len; k++)
-                if (a[i].s[k] == '9') a[i].s[k] = '6';
+                if (a[i].s[k] == '9')
+                  a[i].s[k] = '6';
               break;
-            } else {
-              if (a[i].s[j] == '6' && a[i - 1].s[j] == '9') {
+            }
+            else
+            {
+              if (a[i].s[j] == '6' && a[i - 1].s[j] == '9')
+              {
                 a[i].s[j] = '9';
-              } else if (a[i].s[j] == '6' && a[i - 1].s[j] != '9') {
+              }
+              else if (a[i].s[j] == '6' && a[i - 1].s[j] != '9')
+              {
                 a[i].s[j] = '9';
                 for (int k = j + 1; k < a[i].len; k++)
-                  if (a[i].s[k] == '9') a[i].s[k] = '6';
+                  if (a[i].s[k] == '9')
+                    a[i].s[k] = '6';
                 break;
-              } else {
-                if (pos != -1) {
+              }
+              else
+              {
+                if (pos != -1)
+                {
                   a[i].s[pos] = '9';
                   for (int k = j + 1; k < a[i].len; k++)
-                    if (a[i].s[k] == '9') a[i].s[k] = '6';
+                    if (a[i].s[k] == '9')
+                      a[i].s[k] = '6';
                   break;
-                } else {
+                }
+                else
+                {
                   flag = 0;
                   break;
                 }
@@ -68,10 +103,14 @@ int main() {
       }
     }
   }
-  if (flag) {
+  if (flag)
+  {
     cout << "possible" << '\n';
-    for (int i = 0; i < n; i++) cout << a[i].s << '\n';
-  } else {
+    for (int i = 0; i < n; i++)
+      cout << a[i].s << '\n';
+  }
+  else
+  {
     cout << "impossible" << '\n';
   }
   return 0;
